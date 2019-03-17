a74c944bbc4194fe244c3f6b1647805c7ce8ea23
gralbert <albertgralin@gmail.com>
2019-03-17 16:53:10 +0700

    Add Minkovskii curve + user input

--- main1.py
+++ main1.py
@@ -7,25 +7,18 @@ import rulocal as ru
 import turtle as trt
 
 
-def flower(d, n):
-    """ Draw a flower. """
-    # TODO
+def sq(d, n):
+    """ Draw the squares. """
     if n == 0:
         return
-    trt.forward(100)
-    trt.right(45)
-    trt.forward(50)
-    trt.right(180)
-    trt.forward(50)
-    trt.right(90)
-    trt.forward(50)
-    trt.left(180)
-    trt.forward(50)
-    trt.right(45)
-    trt.forward(100)
-    trt.left(180)
-    trt.left(45)
-    tree(d, n-1)
+    trt.up()
+    trt.right(20)
+    trt.forward(d//4)
+    trt.down()
+    for _ in range(4):
+        trt.forward(d)
+        trt.right(90)
+    return sq(d*0.9, n-1)
 
 
 def tree(d, n):
@@ -49,20 +42,6 @@ def tree(d, n):
     tree(d, n-1)
 
 
-def sq(d, n):
-    """ Draw the squares. """
-    if n == 0:
-        return
-    trt.up()
-    trt.right(20)
-    trt.forward(d//4)
-    trt.down()
-    for _ in range(4):
-        trt.forward(d)
-        trt.right(90)
-    return sq(d*0.9, n-1)
-
-
 def branch(n, size):
     """ Draw a fir branch. """
     if n == 0:
@@ -70,7 +49,7 @@ def branch(n, size):
         return
 
     x = size/(n+1)
-    for i in range(n):
+    for i in range(int(n)):
         trt.forward(x)
         trt.left(45)
         branch(n-i-1, 0.5*x*(n-i-1))
@@ -117,49 +96,94 @@ def snowflake(order, size):
         koch(order, size)
 
 
-def main():
-    trt.speed(0)
+def mink(order, size):
+    """ Minkowski curve. """
+    if order == 0:
+        trt.forward(size)
+    else:
+        mink(order-1, size/3)
+        trt.left(90)
+        mink(order-1, size/3)
+        trt.right(90)
+        mink(order-1, size/3)
+        trt.right(90)
+        mink(order-1, size/3)
+        mink(order - 1, size / 3)
+        trt.left(90)
+        mink(order - 1, size / 3)
+        trt.left(90)
+        mink(order - 1, size / 3)
+        trt.right(90)
+        mink(order - 1, size / 3)
 
-    screen = trt.getscreen()
-    answer = screen.textinput("Welcome!", "Choose fractal (1, 2 ... or 5).")
 
-    if answer is None or answer.lower().startswith('1'):
-        sq(100, 20)
-        trt.done()
-    elif answer.lower().startswith('2'):
+def place(num):
+    """ Put the turtle in the right place. """
+    if num in {'3','2', '1'}:
+        trt.up()
+        trt.goto(0, -100)
+        trt.down()
+        trt.left(90)
+    elif num == '4':
         trt.up()
         trt.goto(-300, 0)
         trt.down()
-        koch(5, 5000)
-        trt.done()
-    elif answer.lower().startswith('3'):
+    elif num == '5':
         trt.up()
         trt.goto(-100, 0)
         trt.down()
-        snowflake(3, 100)
-        trt.done()
-    elif answer.lower().startswith('3'):
+    elif num == '6':
         trt.up()
-        trt.goto(0, -100)
-        trt.left(90)
+        trt.backward(200)
         trt.down()
-        branch(2, 400)
-        trt.done()
-    elif answer.lower().startswith('4'):
-        trt.up()
-        trt.goto(0, -100)
-        trt.down()
-        trt.left(90)
-        tree(10, 8)
-        trt.done()
-    elif answer.lower().startswith('5'):
-        trt.up()
-        trt.goto(0, -100)
-        trt.down()
-        trt.left(90)
-        flower(10, 8)
-        trt.done()
 
+
+def get_data(_size, _depth):
+    """ Get data for drawing. """
+    screen2 = trt.getscreen()
+    size = screen2.numinput(ru.DATA, ru.SIZE, _size, minval=10, maxval=10000)
+    depth = screen2.numinput(ru.DATA, ru.DEPTH, _depth, minval=0, maxval=1000)
+
+    return {'size': size, 'depth': depth}
+
+
+def main():
+    """ Main function. """
+    trt.speed(0)
+
+    screen = trt.getscreen()
+    answer = screen.textinput('Welcome!', ru.WELCOME)
+
+    if answer.startswith('1') or answer is None:
+        data = get_data(100, 20)
+        place('1')
+        sq(data['size'], data['depth'])
+        trt.done()
+    elif answer.startswith('2'):
+        data = get_data(100, 20)
+        place('2')
+        tree(data['size'], data['depth'])
+        trt.done()
+    elif answer.startswith('3'):
+        data = get_data(400, 3)
+        place('3')
+        branch(data['depth'], data['size'])
+        trt.done()
+    elif answer.startswith('4'):
+        data = get_data(500, 3)
+        place('4')
+        koch(data['depth'], data['size'])
+        trt.done()
+    elif answer.startswith('5'):
+        data = get_data(100, 3)
+        place('5')
+        snowflake(data['depth'], data['size'])
+        trt.done()
+    elif answer.startswith('6'):
+        data = get_data(200, 3)
+        place('6')
+        mink(data['depth'], data['size'])
+        trt.done()
 
 if __name__ == '__main__':
     main()
